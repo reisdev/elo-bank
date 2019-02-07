899a43bda9cc6a2f1ab413970de1fa1c91952b6e
Matheus dos Reis <matheus.reis@nobugs.com.br>
2019-02-06 22:07:06 -0200

    Updates

---
+++ precache-manifest.57a37c9c39f9b95c80dfe46500940e96.js
@@ -0,0 +1,90 @@
+self.__precacheManifest = [
+  {
+    "revision": "cd6c777f1945164224dee082abaea03a",
+    "url": "/elo-bank/static/media/outline-icons.cd6c777f.woff2"
+  },
+  {
+    "revision": "5625584dbd7f425af729",
+    "url": "/elo-bank/static/css/main.1943b068.chunk.css"
+  },
+  {
+    "revision": "a1a749e89f578a49306ec2b055c073da",
+    "url": "/elo-bank/static/media/brand-icons.a1a749e8.svg"
+  },
+  {
+    "revision": "d5d41ea824d016021b75",
+    "url": "/elo-bank/static/js/1.d5d41ea8.chunk.js"
+  },
+  {
+    "revision": "43a01473c93e733124d2",
+    "url": "/elo-bank/static/js/runtime~main.43a01473.js"
+  },
+  {
+    "revision": "9c74e172f87984c48ddf5c8108cabe67",
+    "url": "/elo-bank/static/media/flags.9c74e172.png"
+  },
+  {
+    "revision": "8e3c7f5520f5ae906c6cf6d7f3ddcd19",
+    "url": "/elo-bank/static/media/icons.8e3c7f55.eot"
+  },
+  {
+    "revision": "0ab54153eeeca0ce03978cc463b257f7",
+    "url": "/elo-bank/static/media/icons.0ab54153.woff2"
+  },
+  {
+    "revision": "faff92145777a3cbaf8e7367b4807987",
+    "url": "/elo-bank/static/media/icons.faff9214.woff"
+  },
+  {
+    "revision": "b87b9ba532ace76ae9f6edfe9f72ded2",
+    "url": "/elo-bank/static/media/icons.b87b9ba5.ttf"
+  },
+  {
+    "revision": "701ae6abd4719e9c2ada3535a497b341",
+    "url": "/elo-bank/static/media/outline-icons.701ae6ab.eot"
+  },
+  {
+    "revision": "5625584dbd7f425af729",
+    "url": "/elo-bank/static/js/main.5625584d.chunk.js"
+  },
+  {
+    "revision": "ef60a4f6c25ef7f39f2d25a748dbecfe",
+    "url": "/elo-bank/static/media/outline-icons.ef60a4f6.woff"
+  },
+  {
+    "revision": "ad97afd3337e8cda302d10ff5a4026b8",
+    "url": "/elo-bank/static/media/outline-icons.ad97afd3.ttf"
+  },
+  {
+    "revision": "82f60bd0b94a1ed68b1e6e309ce2e8c3",
+    "url": "/elo-bank/static/media/outline-icons.82f60bd0.svg"
+  },
+  {
+    "revision": "962a1bf31c081691065fe333d9fa8105",
+    "url": "/elo-bank/static/media/icons.962a1bf3.svg"
+  },
+  {
+    "revision": "13db00b7a34fee4d819ab7f9838cc428",
+    "url": "/elo-bank/static/media/brand-icons.13db00b7.eot"
+  },
+  {
+    "revision": "e8c322de9658cbeb8a774b6624167c2c",
+    "url": "/elo-bank/static/media/brand-icons.e8c322de.woff2"
+  },
+  {
+    "revision": "a046592bac8f2fd96e994733faf3858c",
+    "url": "/elo-bank/static/media/brand-icons.a046592b.woff"
+  },
+  {
+    "revision": "c5ebe0b32dc1b5cc449a76c4204d13bb",
+    "url": "/elo-bank/static/media/brand-icons.c5ebe0b3.ttf"
+  },
+  {
+    "revision": "d5d41ea824d016021b75",
+    "url": "/elo-bank/static/css/1.cfb218b7.chunk.css"
+  },
+  {
+    "revision": "31a25636729b08d0d438ee8733df1582",
+    "url": "/elo-bank/index.html"
+  }
+];
